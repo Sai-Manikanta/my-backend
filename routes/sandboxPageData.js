0cc8a574832756f35bdc59eb608458835da7ef51
Sai Manikanta Bandaru <sai@rejola.com>
2024-06-27 15:21:47 +0530

    removed cms sandbox data edit api

--- routes/sandboxPageData.js
+++ routes/sandboxPageData.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { createSandboxPageData, getSandboxPageData, updateSandboxData } = require('../controllers/sandboxPageData.js');
+const { createSandboxPageData, getSandboxPageData } = require('../controllers/sandboxPageData.js');
 
 router.post('/', createSandboxPageData);
 router.get('/:queryApi', getSandboxPageData);
-router.patch('/responseParameters/:name', updateSandboxData);
 
 module.exports = router;
